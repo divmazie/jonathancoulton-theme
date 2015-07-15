afdf87c456ce3c7e20a5ba683df3ea0715b268f1
David Mackenzie <davidamackenzie@gmail.com>
2015-07-15 16:07:36 -0400

    More bootstrap classes

--- functions.php
+++ functions.php
@@ -53,4 +53,4 @@ function myfoo( $text ) {
 	return $text;
 }
 
-wp_enqueue_style('bootstrap',get_template_directory_uri().'css/bootstrap.css');
+wp_enqueue_style('bootstrap',get_template_directory_uri().'/css/bootstrap.css');
