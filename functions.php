45a9f3a553b85968f827f1847e8ef53ba61fbd26
David Mackenzie <davidamackenzie@gmail.com>
2015-07-21 18:44:46 -0400

    ACF fields loaded from json

--- functions.php
+++ functions.php
@@ -54,9 +54,11 @@ function myfoo( $text ) {
 }
 
 wp_enqueue_style('bootstrap',get_template_directory_uri().'/css/bootstrap.css');
-if(!is_admin() && !is_login_page()) {
+if(!is_admin()) {
 	wp_enqueue_script('bower', get_template_directory_uri() . '/js/bower.min.js');
 	wp_enqueue_script('site', get_template_directory_uri() . '/js/site.js');
+	wp_dequeue_script('jquery');
+	wp_dequeue_script('jquery-core');
 }
 
 if( function_exists('acf_add_options_page') ) {
@@ -66,46 +68,5 @@ if( function_exists('acf_add_options_page') ) {
 		'menu_slug' => 'theme-general-settings',
 		'capability' => 'edit_posts',
 		'redirect' => false
-	));
-	acf_add_local_field_group(array (
-		'key' => 'group_55ad56ef6b3ea',
-		'title' => 'Options',
-		'fields' => array (
-			array(
-				'key' => 'field_54b83b55ed730',
-				'label' => 'Footer left column header',
-				'name' => 'footer_left_header',
-				'type' => 'text',
-				'instructions' => '',
-				'required' => 0,
-				'default_value' => 'Contact / Press',
-				'placeholder' => ''
-			),
-			array(
-				'key' => 'field_54b83bbced731',
-				'label' => 'Footer left column text',
-				'name' => 'footer_left_text',
-				'type' => 'text',
-				'instructions' => '',
-				'required' => 0,
-				'default_value' => '',
-				'placeholder' => ''
-			)
-		),
-		'location' => array (
-			array (
-				array (
-					'param' => 'options_page',
-					'operator' => '==',
-					'value' => 'theme-general-settings',
-				),
-			),
-		),
-		'menu_order' => 1,
-		'position' => 'normal',
-		'style' => 'seamless',
-		'label_placement' => 'top',
-		'instruction_placement' => 'label',
-		'hide_on_screen' => '',
 	));
 }
