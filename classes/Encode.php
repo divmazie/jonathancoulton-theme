d1c53760e4bd7b26934fcebe0c37aa7028861418
Drew Westphal <drewwestphal@users.noreply.github.com>
2015-07-26 12:53:37 -0400

    expl comment improve

--- classes/Encode.php
+++ classes/Encode.php
@@ -30,7 +30,7 @@ class Encode extends WordpressFileAsset {
         // remove non ascii alnum_ with
         $title = preg_replace('/[^\da-z_]/i', '', $title);
 
-        // track number underscore track title dot extension
+        // track number underscore track title underscore short hash dot extension
         return sprintf('%d_%s_%s.%s', $this->parentTrack->getTrackNumber(),
                        $this->getShortEncodeHash(),
                        $title, $this->encodeFormat);
