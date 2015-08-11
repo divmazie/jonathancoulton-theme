6f117f722601a1a6c520f3a6a330a18e0ab87ba5
David Mackenzie <davidamackenzie@gmail.com>
2015-08-11 17:58:41 -0400

    Only zip album if it is zip worthy

--- classes/AlbumZip.php
+++ classes/AlbumZip.php
@@ -71,44 +71,43 @@ class AlbumZip extends WordpressFileAsset {
     }
 
     public function createZip() {
-        $upload_dir = wp_upload_dir();
-        $zip = new \ZipArchive();
-        $filename = $upload_dir['path']."/".$this->getFileAssetFileName();
-        $filetype = wp_check_filetype( basename( $filename ), null );
-        if ($zip->open($filename, \ZipArchive::CREATE)!==TRUE) {
-            exit("cannot open <$filename>\n");
-        }
-        $zip_dir_name = $this->parentAlbum->getAlbumTitle()."/";
-        //$test_filename = $upload_dir['path']."/270924_10150965531319525_1326549980_n-150x150.jpg";
-        //$zip->addFile($test_filename,$zip_dir_name.basename($test_filename));
-        foreach ($this->getEncodes() as $encode) {
-            $encode_path = $encode->getPath();
-            if ($encode_path) {
-                $zip->addFile($encode_path, $zip_dir_name . basename($encode_path));
+        if ($this->isZipWorthy()) {
+            $upload_dir = wp_upload_dir();
+            $zip = new \ZipArchive();
+            $filename = $upload_dir['path'] . "/" . $this->getFileAssetFileName();
+            $filetype = wp_check_filetype(basename($filename), null);
+            if ($zip->open($filename, \ZipArchive::CREATE) !== TRUE) {
+                exit("cannot open <$filename>\n");
             }
+            $zip_dir_name = $this->parentAlbum->getAlbumTitle() . "/";
+            foreach ($this->getEncodes() as $encode) {
+                $encode_path = $encode->getPath();
+                if ($encode_path) {
+                    $zip->addFile($encode_path, $zip_dir_name . basename($encode_path));
+                }
+            }
+            $bonus_path = $this->parentAlbum->getAlbumBonusAssetPath();
+            if ($bonus_path) {
+                $zip->addFile($bonus_path, $zip_dir_name . basename($bonus_path));
+            }
+            $zip->close();
+            $attachment = array(
+                'guid' => $upload_dir['url'] . '/' . basename($filename),
+                'post_mime_type' => $filetype['type'],
+                'post_title' => preg_replace('/\.[^.]+$/', '', basename($filename)),
+                'post_content' => '',
+                'post_status' => 'inherit'
+            );
+            // Insert the attachment.
+            $attach_id = wp_insert_attachment($attachment, $filename, $this->parentAlbum->getPostID());
+            // Make sure that this file is included, as wp_generate_attachment_metadata() depends on it.
+            require_once(ABSPATH . 'wp-admin/includes/image.php');
+            // Generate the metadata for the attachment, and update the database record.
+            $attach_data = wp_generate_attachment_metadata($attach_id, $filename);
+            $attach_data = array_merge($attach_data, array('unique_key' => $this->getUniqueKey()));
+            wp_update_attachment_metadata($attach_id, $attach_data);
+            $this->setWPAttachmentID($attach_id);
         }
-        $bonus_path = $this->parentAlbum->getAlbumBonusAssetPath();
-        if ($bonus_path) {
-            $zip->addFile($bonus_path, $zip_dir_name . basename($bonus_path));
-        }
-        $zip->close();
-        //return $zip;
-        $attachment = array(
-            'guid'           => $upload_dir['url'] . '/' . basename( $filename ),
-            'post_mime_type' => $filetype['type'],
-            'post_title'     => preg_replace( '/\.[^.]+$/', '', basename( $filename ) ),
-            'post_content'   => '',
-            'post_status'    => 'inherit'
-        );
-        // Insert the attachment.
-        $attach_id = wp_insert_attachment( $attachment, $filename, $this->parentAlbum->getPostID() );
-        // Make sure that this file is included, as wp_generate_attachment_metadata() depends on it.
-        require_once( ABSPATH . 'wp-admin/includes/image.php' );
-        // Generate the metadata for the attachment, and update the database record.
-        $attach_data = wp_generate_attachment_metadata( $attach_id, $filename );
-        $attach_data = array_merge($attach_data, array('unique_key' => $this->getUniqueKey()));
-        wp_update_attachment_metadata( $attach_id, $attach_data );
-        $this->setWPAttachmentID($attach_id);
     }
 
     public function getParentAlbum() {
