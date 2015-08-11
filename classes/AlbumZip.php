115e414c0da705858f709082e5fbbe8ef6ba04f2
David Mackenzie <davidamackenzie@gmail.com>
2015-08-11 16:02:22 -0400

    AlbumZip creates zip file in uploads dir

--- classes/AlbumZip.php
+++ classes/AlbumZip.php
@@ -35,7 +35,16 @@ class AlbumZip extends WordpressFileAsset {
     }
 
     public function getFileAssetFileName() {
+        //return "something.zip";
+        $title = $this->parentAlbum->getAlbumTitle();
+        $title = iconv('UTF-8','ASCII//TRANSLIT',$title);
+        // replace spaces with underscore
+        $title = preg_replace('/\s/u', '_', $title);
+        // remove non ascii alnum_ with
+        $title = preg_replace('/[^\da-z_]/i', '', $title);
 
+        // album title underscore short hash dot extension
+        return sprintf('%s_%s.%s',$title,$this->getShortUniqueKey(),"zip");
     }
 
     public function getEncodes() {
@@ -49,15 +58,39 @@ class AlbumZip extends WordpressFileAsset {
     }
 
     public function createZip() {
-        //return "before zip construct";
-        $zip = new ZipArchive();
-        return "after zip construct";
-        $filename = "./test112.zip";
-        return $filename;
-        if ($zip->open($filename, ZipArchive::CREATE)!==TRUE) {
+        $upload_dir = wp_upload_dir();
+        $zip = new \ZipArchive();
+        $filename = $upload_dir['path']."/".$this->getFileAssetFileName();
+        $filetype = wp_check_filetype( basename( $filename ), null );
+        if ($zip->open($filename, \ZipArchive::CREATE)!==TRUE) {
             exit("cannot open <$filename>\n");
         }
-        return $zip;
+        $zip_dir_name = $this->parentAlbum->getAlbumTitle()."/";
+        //$test_filename = $upload_dir['path']."/270924_10150965531319525_1326549980_n-150x150.jpg";
+        //$zip->addFile($test_filename,$zip_dir_name.basename($test_filename));
+        foreach ($this->getEncodes() as $encode) {
+            $encode_path = $encode->getPath();
+            if ($encode_path) {
+                $zip->addFile($encode_path, $zip_dir_name . basename($encode_path));
+            }
+        }
+        $zip->close();
+        //return $zip;
+        $attachment = array(
+            'guid'           => $upload_dir['url'] . '/' . basename( $filename ),
+            'post_mime_type' => $filetype['type'],
+            'post_title'     => preg_replace( '/\.[^.]+$/', '', basename( $filename ) ),
+            'post_content'   => '',
+            'post_status'    => 'inherit'
+        );
+        // Insert the attachment.
+        $attach_id = wp_insert_attachment( $attachment, $filename, $this->parentAlbum->getPostID() );
+        // Make sure that this file is included, as wp_generate_attachment_metadata() depends on it.
+        require_once( ABSPATH . 'wp-admin/includes/image.php' );
+        // Generate the metadata for the attachment, and update the database record.
+        $attach_data = wp_generate_attachment_metadata( $attach_id, $filename );
+        wp_update_attachment_metadata( $attach_id, $attach_data );
+        $this->setWPAttachmentID($attach_id);
     }
 
     public function getParentAlbum() {
