6c611c30590234e5eb3d498f0ac103f27510f9b6
David Mackenzie <davidamackenzie@gmail.com>
2015-09-21 19:24:14 -0400

    Shopify product creation method

--- api/store_sync.php
+++ api/store_sync.php
@@ -11,4 +11,17 @@ $apiPassword = get_field('shopify_api_password','options');
 $handle = get_field('shopify_handle','options');
 $shopify = new jct\Shopify($apiKey,$apiPassword,$handle);
 
-echo "<pre>".$shopify->getProducts();
+echo "<pre>";
+//print_r($shopify->getProducts());
+
+$albums = \jct\Album::getAllAlbums();
+foreach ($albums as $album) {
+    $tracks = $album->getAlbumTracks();
+    foreach ($tracks as $track) {
+        $formats = array();
+        foreach ($track->getAllChildEncodes() as $encode) {
+            $formats[] = $encode->getEncodeFormat();
+        }
+        print_r($shopify->createProduct($track->getTrackTitle(),$track->getTrackArtObject()->getURL(),$track->getTrackPrice(),$formats));
+    }
+}
--- classes/Shopify.php
+++ classes/Shopify.php
@@ -20,19 +20,77 @@ class Shopify {
 
     public function makeCall($resource, $requestType = "GET", $args=array()) {
         $url = "https://".$this->apiKey.":".$this->apiPassword."@".$this->handle.".myshopify.com/".$resource.".json";
+        if ($requestType == "GET") {
+            $first = true;
+            foreach ($args as $key => $val) {
+                $key = urlencode($key);
+                $val = urlencode($val);
+                if ($first) {
+                    $url .= "?$key=$val";
+                } else {
+                    $url .= "&$key=$val";
+                }
+                $first = false;
+            }
+        }
         $data_string = json_encode($args);
+        //return $data_string;
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $requestType);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+                'Content-Type: application/json',
+                'Content-Length: ' . strlen($data_string),
+                'Expect:'
+            ]
+        );
         if ($requestType != "GET") {
             curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
         }
         $response = curl_exec($ch);
-        return $response;
+        return json_decode($response);
     }
 
     public function getProducts() {
         return $this->makeCall("admin/products");
+    }
+
+    public function createProduct($title,$image_src,$price,$formats) {
+        $sku = "999";
+        $variants = array();
+        foreach ($formats as $format) {
+            $variants[] = array(
+                'option1' => $format,
+                'price' => strval($price),
+                'sku' => $sku,
+                'taxable' => false,
+                'requires_shipping' => false
+            );
+        }
+        $args = array("product" => array(
+            'title' => $title,
+            'body_html' => "$title by Jonathan Coulton",
+            'vendor' => "Jonathan Coulton",
+            'product_type' => 'Music download',
+            'images' => array(
+                array('src' => $image_src)
+            ),
+            'variants' => $variants
+        ));
+        //return json_encode($args);
+        $response = $this->makeCall("admin/products","POST",$args);
+        //$this->attachImage($response->product->id,$image_src);
+        return $response;
+        if ($response->product->id) { // I'm in the middle of working out an image upload bug here
+            return $this->attachImage($response->product->id,$image_src);
+        } else {
+            return $response;
+        }
+    }
+
+    public function attachImage($productId,$image_src) {
+        $args = array('image' => array('src' => $image_src));
+        return $this->makeCall("admin/products/#$productId/images","POST",$args);
     }
 
 }
