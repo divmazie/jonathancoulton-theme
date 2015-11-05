5be9ba2ec082a78e2c971dadc533da7d68523397
David Mackenzie <davidamackenzie@gmail.com>
2015-11-05 18:14:06 -0500

    More styling: Nav, promo boxes, headers, custom bootstrap reorganize, images

--- include/sitewide_context.php
+++ include/sitewide_context.php
@@ -10,6 +10,7 @@ if (!isset($context)) {
     $context = array();
 }
 
+$context['is_logged_in'] = is_user_logged_in();
 $context['faqs'] = Timber::get_posts('post_type=faq');
 $context['archives'] = Timber::get_posts('numberposts=3');
 $context['get_vars'] = $_GET;
