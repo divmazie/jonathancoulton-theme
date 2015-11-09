3ea920ad233acb0ee22f9d5b1473f225ffa39974
David Mackenzie <davidamackenzie@gmail.com>
2015-11-09 14:29:32 -0500

    Style tweaks: nav border, sidebar layout, arrow hover

--- config/instagram.php
+++ config/instagram.php
@@ -6,6 +6,8 @@
  * Time: 13:35
  */
 
+$number_images = 3;
+
 // Return cached tweets if we have them, doesn't call api more than once per minute
 $from_transient = get_transient('instagram_context');
 if ($from_transient) {
@@ -18,7 +20,7 @@ $handle = get_field('instagram_handle','options');
 $user_id = 1593277106; // This is a realjonathancoulton's user-id hardcoded, see http://jelled.com/instagram/lookup-user-id#
 $client_id = get_field('instagram_client_id','option'); // API key was set up by David (Instagram: divmazie), get from him or register new one
 $instagram = new Instagram($client_id);
-$response = $instagram->getUserMedia($user_id,4);
+$response = $instagram->getUserMedia($user_id,$number_images);
 //return $response->data;
 $media = array();
 foreach ($response->data as $datum) {
