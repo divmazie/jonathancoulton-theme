c200ca49f18aa62cf22852c1e2fc108e415654ff
David Mackenzie <davidamackenzie@gmail.com>
2016-06-15 18:02:28 -0400

    Changes for Timber 1.0

--- include/routes.php
+++ include/routes.php
@@ -13,24 +13,24 @@ function authcode_valid($code) { // Check against transient with encoder validat
         return false; // Change this to false when actually want to test!
 }
 
-Timber::add_route('api/:authcode/:script/:var', function($params){
+Routes::map('api/:authcode/:script/:var', function($params){
     if (authcode_valid($params['authcode'])) {
         include get_template_directory()."/api/".$params['script'].".php";
         die();
     } else {
-        Timber::load_template("404.php");
+        Routes::load("404.php");
     }
 });
 
-Timber::add_route('custom_admin/:script', function($params){
+Routes::map('custom_admin/:script', function($params){
     if (current_user_can('manage_options')) {
-        Timber::load_template('custom_admin/'.$params['script'].".php");
+        Routes::load('custom_admin/'.$params['script'].".php");
     } else {
-        Timber::load_template("404.php");
+        Routes::load("404.php");
     }
 });
 
-Timber::add_route('wiki/:wikipage', function($params) {
+Routes::map('wiki/:wikipage', function($params) {
     $redirect_location = get_field('joco_wiki_base_url','options').$params['wikipage'];
     header('Location: '.$redirect_location,TRUE,301);
     die();
